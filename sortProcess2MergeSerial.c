716250c5905f5562e7f064c5e3c6365c6ecda275
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2022-06-19 19:08:09 -0300

    sortProcess2MergeSerial VALIDO

--- sortProcess2MergeSerial.c
+++ sortProcess2MergeSerial.c
@@ -9,9 +9,59 @@
 
 #include "common.h"
 
+static int* array;
+static int size;
+static int* newArray;
+
+static void* sort1(void* arg) {
+    bubbleSort(array, size/2);
+    return 0;
+}
+
+static void* sort2(void* arg) {
+    bubbleSort(array+(size/2), size/2);
+    return 0;
+}
+
+static void* merge_wrap(void* arg) {
+    merge(array, newArray, size);
+    return 0;
+}
+
 int main() {
 
-    // COMPLETAR
+    pthread_t thread1, thread2, thread3;
+
+    // Seteo la semilla del generador de numeros aleatorios
+    srand(time(NULL));
+
+    // Genero un arreglo de N elementos
+    size = N;
+    array = arrayNew(size);
+
+    // Ordeno el arreglo en dos procesos distintos
+    pthread_create(&thread1, NULL, sort1, NULL);
+    pthread_create(&thread2, NULL, sort2, NULL);
+
+    // Espero la terminacion de los dos procesos
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    newArray = (int*)malloc(sizeof(int)*size);
+
+    pthread_create(&thread3, NULL, merge_wrap, NULL);
+
+    pthread_join(thread3, NULL);
+
+    arrayPrint(newArray, size);
+
+    
+    // Verificar si el arreglo esta ordenado
+    arraySortCheck(newArray, size);
+    
+    // Libero memoria
+    free(array);
+    free(newArray);
 
     return 0;
 }
